77235b89bde0414b339c45800c1e63b36422d6d5
Anastasia Sokolova <nsokolova0399@mail.ru>
2022-03-01 22:48:11 +0300

    Second1

--- hello.js
+++ hello.js
@@ -1,13 +1,10 @@
-
 var readline = require('readline');
-
 var rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 let total = 0;
 process.stdin.on('end', () => { console.log(total); process.exit(0); });
-
 rl.on('line', function (data) {
     data = data.split(' ');
     let result = 0;
@@ -32,7 +29,6 @@ function checkList(list){
     }
     return result;
 }
-
 function Task(list){
     let result = 0;
     let a = Number(list[0])
