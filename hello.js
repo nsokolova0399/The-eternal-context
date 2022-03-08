d6054a77ae3355f2a9f38edc4d801889e4ac2cc2
Anastasia Sokolova <nsokolova0399@mail.ru>
2022-03-08 13:36:36 +0300

    finish

--- hello.js
+++ hello.js
@@ -1,46 +1,49 @@
-// var readline = require('readline');
-// var rl = readline.createInterface({
-//     input: process.stdin,
-//     output: process.stdout
-// });
-// let total = 0;
-// process.stdin.on('end', () => { console.log(total); process.exit(0); });
-// rl.on('line', function (data) {
-//     data = data.split(' ');
-//     let result = 0;
-//     if (checkList(data)){
-//         result = Task(data);
-//         console.log(result);
-//     }
-// });
-// function checkList(list){
-//     let result = false;
-//     let k = 0;
-//     for(let i = 0; i < list.length; i++){
-//        if (Number(list[i])<=100 && Number(list[i])>=1){
-//            k++;
-//        }
-//     }
-//     if (k === list.length){
-//         result = true;
-//     }
-//     if (list.length !== 4){
-//         result = false;
-//     }
-//     return result;
-// }
-// function Task(list){
-//     let result = 0;
-//     let a = Number(list[0])
-//     let b = Number(list[1])
-//     let c = Number(list[2])
-//     let d = Number(list[3])
-//     if (b >= d){
-//         result = a;
-//     }
-//     else if (b < d){
-//         result = c * (d-b) + a;
-//     }
-//     return result;
-// }
+var readline = require('readline');
+var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+let total = 0;
+
+rl.on('line', function (data) {
+    data = data.split(' ');
+    let result = 0;
+    if (checkList(data) && data.length===4){
+        result = Task(data);
+        console.log(result);
+        rl.close();
+    }
+}).on('close', (total)=> process.exit(0));
+
+function checkList(list){
+    let result = false;
+    let k = 0;
+    for(let i = 0; i < list.length; i++){
+        if (Number(list[i])<=100 && Number(list[i])>=1){
+            k++;
+        }
+    }
+    if (k === list.length){
+        result = true;
+    }
+    if (list.length !== 4){
+        result = false;
+    }
+    return result;
+}
+
+function Task(list){
+    let result = 0;
+    let a = Number(list[0])
+    let b = Number(list[1])
+    let c = Number(list[2])
+    let d = Number(list[3])
+    if (b >= d){
+        result = a;
+    }
+    else if (b < d){
+        result = c * (d-b) + a;
+    }
+    return result;
+}
 
--- hello1.js
+++ hello1.js
@@ -4,10 +4,11 @@ var rl = readline.createInterface({
     output: process.stdout
 });
 let total = 0;
-process.stdin.on('end', () => { console.log(total); process.exit(0); });
+
 rl.on('line', function (data) {
+    data = data.split('/n');
     if (data.length === 1){
-        let n = Number(data[0]);
+        let n = parseInt(data);
         if (n >=1 || n <= 2000000000){
             let i = 0;
             while(n !== 1){
@@ -15,8 +16,9 @@ rl.on('line', function (data) {
                 i++;
             }
            console.log(i);
+            rl.close();
         }
     }
-});
+}).on('close', (total)=> process.exit(0));
 
 
